253590fe7df9eb86f69cd6cf35b33e2d9b40abed
huyenthunenha <126221669+huyenthunenha@users.noreply.github.com>
2024-10-31 00:30:19 +0700

    Sửa lại filter type

--- src/pages/client/move.js
+++ src/pages/client/move.js
@@ -75,7 +75,7 @@ const Move = () => {
     };
 
     const handleTypeChange = (e) => {
-        setSelectedType(e.target.value);
+        const type = e.target.value;
         setSelectedType(type);
 
         const filtered = moves.filter(move => {
@@ -85,7 +85,6 @@ const Move = () => {
         });
         setFilteredMoves(filtered);
     }
-
     //Gọi API
     useEffect(() => {
         const fetchMoves = async () => {
@@ -130,32 +129,32 @@ const Move = () => {
                         placeholder="Search by name"
                         className="w-60 h-10 border border-neutral-300 rounded p-2 focus:outline-none focus:ring-1 focus:ring-blue-400 ml-3"
                     />
-                    {/* <label htmlFor="filter-move-type" className="ml-5">Type:</label>
+                    <label htmlFor="filter-move-type" className="ml-5">Type:</label>
                     <select id="filter-move-type"
                             value={selectedType}
                             onChange={handleTypeChange} 
                             className="w-24 h-10 border border-neutral-300 rounded p-2 focus:outline-none focus:ring-1 focus:ring-blue-400 ml-3"
                             required>
-                        <option value=''>- All -</option>
-                        <option value='normal'>Normal</option>
-                        <option value='fire'>Fire</option>
-                        <option value='water'>Water</option>
-                        <option value='electric'>Electric</option>
-                        <option value='grass'>Grass</option>
-                        <option value='ice'>Ice</option>
-                        <option value='fighting'>Fighting</option>
-                        <option value='poison'>Poison</option>
-                        <option value='ground'>Ground</option>
-                        <option value='flying'>Flying</option>
-                        <option value='psychic'>Psychic</option>
-                        <option value='bug'>Bug</option>
-                        <option value='rock'>Rock</option>
-                        <option value='ghost'>Ghost</option>
-                        <option value='dragon'>Dragon</option>
-                        <option value='dark'>Dark</option>
-                        <option value='steel'>Steel</option>
-                        <option value='fairy'>Fairy</option>
-                    </select> */}
+                        <option value='All'>- All -</option>
+                        <option value='Normal'>Normal</option>
+                        <option value='Fire'>Fire</option>
+                        <option value='Water'>Water</option>
+                        <option value='Electric'>Electric</option>
+                        <option value='Grass'>Grass</option>
+                        <option value='Ice'>Ice</option>
+                        <option value='Fighting'>Fighting</option>
+                        <option value='Poison'>Poison</option>
+                        <option value='Ground'>Ground</option>
+                        <option value='Flying'>Flying</option>
+                        <option value='Psychic'>Psychic</option>
+                        <option value='Bug'>Bug</option>
+                        <option value='Rock'>Rock</option>
+                        <option value='Ghost'>Ghost</option>
+                        <option value='Dragon'>Dragon</option>
+                        <option value='Dark'>Dark</option>
+                        <option value='Steel'>Steel</option>
+                        <option value='Fairy'>Fairy</option>
+                    </select>
                     {/* <label htmlFor="filter-move-category" className="ml-5">Category:</label>
                     <select id="filter-move-category"
                             className="w-24 h-10 border border-neutral-300 rounded p-2 focus:outline-none focus:ring-1 focus:ring-blue-400 ml-3"
